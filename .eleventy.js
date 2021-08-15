ca085237459332e310704c6a7124699fbf5c39ce
kamst01 <kamste01@gmail.com>
2021-08-14 21:43:04 -0400

    netlifycms alignment changes.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,4 @@
 module.exports = function(eleventyConfig) {
     let handlebars = require("handlebars");
-
     eleventyConfig.setLibrary("hbs", handlebars);
-    eleventyConfig.addPassthroughCopy({
-      "./admin/config.yml": "./admin/config.yml",
-    });
 };
