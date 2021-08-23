d4ba925a01644fd0a14abc97c76bba52782756d0
kamst01 <kamste01@gmail.com>
2021-08-23 11:43:15 -0400

    remove existing content data.

--- .eleventy.js
+++ .eleventy.js
@@ -2,15 +2,9 @@ module.exports = function(eleventyConfig) {
     // import handlebarsjs template language
     let handlebars = require("handlebars");
     eleventyConfig.setLibrary("hbs", handlebars);
-    // passthrough admin folder to the _site folder on build
-    eleventyConfig.addPassthroughCopy("admin");
     // import markdown-it, to read the markdown from netlifycms in index.md
     let markdownIt = require("markdown-it");
-    let options = {
-        html: true,
-        breaks: true,
-        linkify: true
-    };
-    
-    eleventyConfig.setLibrary("md", markdownIt(options));
+    eleventyConfig.setLibrary("md", markdownIt);
 };
+    // passthrough admin folder to the _site folder on build
+    eleventyConfig.addPassthroughCopy("admin");
