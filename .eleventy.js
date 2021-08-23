29df2fcc1ef888ae3da3d4c8fde2f8474d54c9b0
kamst01 <kamste01@gmail.com>
2021-08-23 13:45:32 -0400

    add content.

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,6 @@ module.exports = function(eleventyConfig) {
     // import handlebarsjs template language
     let handlebars = require("handlebars");
     eleventyConfig.setLibrary("hbs", handlebars);
-    // import markdown-it, to read the markdown from netlifycms in index.md
-    let markdownIt = require("markdown-it");
-    eleventyConfig.setLibrary("md", markdownIt);
     // passthrough admin folder to the _site folder on build
     eleventyConfig.addPassthroughCopy("admin");
 };
