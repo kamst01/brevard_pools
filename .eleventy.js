232c25eeb739e6563baee35bdc8e14f183ab0afd
kamst01 <kamste01@gmail.com>
2021-09-21 15:42:47 -0400

    add jszip to the passthroughcopy.

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,6 @@ module.exports = function(eleventyConfig) {
     // import handlebarsjs template language
     let handlebars = require("handlebars");
     eleventyConfig.setLibrary("hbs", handlebars);
+    eleventyConfig.addPassthroughCopy({"node_modules/jszip/dist/jszip.min.js": "jszip.min.js"});
     eleventyConfig.addPassthroughCopy({"_includes/form.11ty.js": "script.js"});
 };
--- _includes/form.11ty.js
+++ _includes/form.11ty.js
@@ -1,5 +1,3 @@
-const JSZip = require("jszip");
-
 let currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
