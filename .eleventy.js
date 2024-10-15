a9d13759872ee0c7d88b25c1331de55621ce2865
Keifer A <amst01k@protonmail.com>
2024-10-14 21:40:41 -0400

    minify html

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,26 @@
+const htmlmin = require("html-minifier-terser");
+let handlebars = require("handlebars");
 module.exports = function(eleventyConfig) {
     // import handlebarsjs template language
-    let handlebars = require("handlebars");
     eleventyConfig.setLibrary("hbs", handlebars);
 
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("admin/config.yml");
+    eleventyConfig.addTransform("htmlmin", function (content) {
+		if ((this.page.outputPath || "").endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+                html5: true,
+                minifyJS: true,
+                processScripts: ['application/ld+json'] 
+			});
+
+			return minified;
+		}
+
+		// If not an HTML output, return content as-is
+		return content;
+	});
 };
